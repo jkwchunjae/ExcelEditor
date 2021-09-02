a42c138bf77e55f51befbf0121eb923362a87d30
jkw <jkwchunjae@daum.net>
2021-09-02 19:30:19 +0900

    Worksheet extensions

---
+++ ExcelEditorAddIn/ExcelEditorAddIn/Extensions/WorksheetExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelEditorAddIn
+{
+    public static class WorksheetExtensions
+    {
+        public static Excel.Range Cell(this Excel.Worksheet sheet, int row1, int column1)
+        {
+            return sheet.Cells[row1, column1];
+        }
+    }
+}
