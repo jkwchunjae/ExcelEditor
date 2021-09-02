56c7535b50e878a44b442f9e174b817ff159f939
jkw <jkwchunjae@daum.net>
2021-09-03 02:33:26 +0900

    BaseWorkbook과 BaseWorksheet를 분리함

--- ExcelEditorAddIn/ExcelEditorAddIn/Workbook/BaseWorkbook.cs
+++ ExcelEditorAddIn/ExcelEditorAddIn/Workbook/BaseWorkbook.cs
@@ -9,12 +9,21 @@ namespace ExcelEditorAddIn
         public string JsonFilePath { get; protected set; }
         public IElement Element { get; protected set; }
         public Excel.Workbook Workbook { get; protected set; }
-        public Excel.Worksheet MainWorksheet { get; protected set;  }
+        public BaseWorksheet MainWorksheet { get; protected set;  }
 
         public BaseWorkbook(IElement element, string jsonFilePath)
         {
             JsonFilePath = jsonFilePath;
             Element = element;
+        }
+
+        public virtual void Open() { }
+
+        protected Excel.Workbook MakeWorkbook()
+        {
+            Workbook = Globals.ThisAddIn.Application.Workbooks.Add();
+
+            return Workbook;
         }
     }
 }
---
+++ ExcelEditorAddIn/ExcelEditorAddIn/Worksheet/TableWorksheet.cs
@@ -0,0 +1,38 @@
+﻿using EeCommon;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelEditorAddIn
+{
+    public class TableWorksheet : BaseWorksheet
+    {
+        ITableElement TableElement { get; }
+        public TableWorksheet(ITableElement element, BaseWorkbook workbook, Excel.Worksheet worksheet)
+            : base(element, workbook, worksheet)
+        {
+            TableElement = element;
+
+            SpreadElement(element);
+        }
+
+        private void SpreadElement(ITableElement tableElement)
+        {
+            var sheet = Worksheet;
+
+            // title
+            for (var column = 1; column <= tableElement.Keys.Count; column++)
+            {
+                var cell = sheet.Cell(1, column);
+                cell.Value2 = tableElement.Keys[column - 1];
+            }
+
+            // values
+            if (tableElement.Any)
+            {
+                var minCell = sheet.Cell(2, 1);
+                var maxCell = sheet.Cell(1 + tableElement.Length, tableElement.Keys.Count);
+                Excel.Range valuesRange = sheet.Range[minCell, maxCell];
+                valuesRange.Value2 = tableElement.Values;
+            }
+        }
+    }
+}
