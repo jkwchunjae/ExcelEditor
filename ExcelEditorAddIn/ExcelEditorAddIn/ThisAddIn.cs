712e9af3cb7246a2ea4de9f54b8102709a9e2a35
jkw <jkwchunjae@daum.net>
2021-09-02 20:12:06 +0900

    중복 열림 방지, 파일 여는 코드를 Ribbon에서 Addin옮김

--- ExcelEditorAddIn/ExcelEditorAddIn/ThisAddIn.cs
+++ ExcelEditorAddIn/ExcelEditorAddIn/ThisAddIn.cs
@@ -7,11 +7,16 @@ using Excel = Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
 using Microsoft.Office.Tools.Excel;
 using System.Windows.Forms;
+using System.IO;
+using EeJson;
+using EeCommon;
 
 namespace ExcelEditorAddIn
 {
     public partial class ThisAddIn
     {
+        List<BaseWorkbook> _workbookData = new List<BaseWorkbook>();
+
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
         }
@@ -19,6 +24,43 @@ namespace ExcelEditorAddIn
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
         }
+
+        private bool AlreadyOpened(string jsonFilePath, out BaseWorkbook workbookData)
+        {
+            var found = _workbookData.FirstOrDefault(x => x.JsonFilePath == jsonFilePath);
+            workbookData = null;
+            if (found != null)
+            {
+                workbookData = found;
+            }
+            return found != null;
+        }
+
+        public void OpenFile(OpenFileDialog openFileDialog)
+        {
+            // TODO: json syntax error
+            var filePath = openFileDialog.FileName;
+
+            if (AlreadyOpened(filePath, out var workbookData))
+            {
+                workbookData.Workbook.Activate();
+                return;
+            }
+
+            using (var reader = new StreamReader(openFileDialog.OpenFile()))
+            {
+                var jsonText = reader.ReadToEnd();
+                var baseDocument = new JsonBaseDocument(jsonText);
+                if (baseDocument.Type == DocumentType.Table)
+                {
+                    var jsonTableDocument = new JsonTableDocument(baseDocument);
+                    var tableWorksheet = new TableWorkbook(jsonTableDocument, filePath);
+                    _workbookData.Add(tableWorksheet);
+                    tableWorksheet.OpenFile();
+                }
+            }
+        }
+
 
         #region VSTO generated code
 
