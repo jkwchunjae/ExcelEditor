7f8d910d36750bb99ad3aa6959df8bfc8d219bc3
jkw <jkwchunjae@daum.net>
2021-09-23 01:38:16 +0900

    #19 우클릭 기반 코드2

---
+++ ExcelEditorAddIn/ExcelEditorAddIn/ContextMenu/ContextMenuFactory.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Office.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelEditorAddIn
+{
+    public static class ContextMenuFactory
+    {
+        private static List<(string MenuName, IContextMenu ContextMenu)> _contextCache = new List<(string MenuName, IContextMenu)>();
+
+        public static ContextMenu_Column CreateColumnMenu(string id)
+        {
+            var menuName = nameof(ContextMenu_Column) + id;
+
+            if (_contextCache.Any(x => x.MenuName == menuName))
+            {
+                return (ContextMenu_Column)_contextCache
+                    .First(x => x.MenuName == menuName)
+                    .ContextMenu;
+            }
+
+            var commandBars = Globals.ThisAddIn.Application.CommandBars;
+            var bar = commandBars.Add(
+                Name: menuName,
+                Position: MsoBarPosition.msoBarPopup,
+                Temporary: true);
+            var menu = new ContextMenu_Column(bar);
+
+            _contextCache.Add((menuName, menu));
+
+            return menu;
+        }
+    }
+}
--- ExcelEditorAddIn/ExcelEditorAddIn/ContextMenu/ContextMenuInfo.cs
+++ ExcelEditorAddIn/ExcelEditorAddIn/ContextMenu/ContextMenuInfo.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using JkwExtensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ExcelEditorAddIn
@@ -25,6 +27,50 @@ namespace ExcelEditorAddIn
             Type = type;
             Address = address;
         }
+
+        public static ContextMenuInfo Make(string address)
+        {
+            // $E$7: SingleCell
+            // $F$4:$G$10: Cells
+            // $C:$C : Column
+            // $1:$1 : Row
+
+            var column = @"\$([A-Za-z]+)";
+            var row = @"\$(\d+)";
+            var cell = $@"{column}{row}"; // \$[A-Za-z]+\$\d+
+
+            var singleCellPattern = $@"^{cell}$";
+            if (Regex.IsMatch(address, singleCellPattern))
+            {
+                return new SingleCellMenuInfo(address);
+            }
+
+            var cellsPattern = $@"^{cell}\:{cell}$"; // ^\$[A-Za-z]+\$\d+\:\$[A-Za-z]+\$\d+$
+            if (Regex.IsMatch(address, cellsPattern))
+            {
+                return new CellsMenuInfo(address);
+            }
+
+            var rowPattern = $@"^{row}\:{row}$"; // ^\$\d+\:\$\d+$
+            if (Regex.IsMatch(address, rowPattern))
+            {
+                var match = Regex.Match(address, rowPattern);
+                var beginRow = match.Groups[1].Value.ToInt();
+                var endRow = match.Groups[2].Value.ToInt();
+                return new RowMenuInfo(address, beginRow, endRow);
+            }
+
+            var columnPattern = $@"^{column}\:{column}$"; // ^\$[A-Za-z]+\:\$[A-Za-z]+$
+            if (Regex.IsMatch(address, columnPattern))
+            {
+                var match = Regex.Match(address, columnPattern);
+                var beginColumn = Utils.ColumnNameToNumber(match.Groups[1].Value);
+                var endColumn = Utils.ColumnNameToNumber(match.Groups[2].Value);
+                return new ColumnMenuInfo(address, beginColumn, endColumn);
+            }
+
+            return new UnknownMenuInfo(address);
+        }
     }
 
     public class SingleCellMenuInfo : ContextMenuInfo
@@ -45,9 +91,9 @@ namespace ExcelEditorAddIn
 
     public class ColumnMenuInfo : ContextMenuInfo
     {
-        public string BeginColumn { get; private set; }
-        public string EndColumn { get; private set; }
-        public ColumnMenuInfo(string address, string beginColumn, string endColumn)
+        public int BeginColumn { get; private set; }
+        public int EndColumn { get; private set; }
+        public ColumnMenuInfo(string address, int beginColumn, int endColumn)
             : base(ContextMenuType.Column, address)
         {
             BeginColumn = beginColumn;
