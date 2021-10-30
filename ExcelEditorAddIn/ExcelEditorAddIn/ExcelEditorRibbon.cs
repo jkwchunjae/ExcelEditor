127da9c9592e6ee6779491af0a5afcf2f5190012
jkw <jkwchunjae@daum.net>
2021-10-30 17:38:08 +0900

    #26 Recents dropdown

--- ExcelEditorAddIn/ExcelEditorAddIn/ExcelEditorRibbon.cs
+++ ExcelEditorAddIn/ExcelEditorAddIn/ExcelEditorRibbon.cs
@@ -11,8 +11,11 @@ namespace ExcelEditorAddIn
 {
     public partial class ExcelEditorRibbon
     {
+        private static readonly string RecentLabel = "Recents";
+
         private void ExcelEditorRibbon_Load(object sender, RibbonUIEventArgs e)
         {
+            InitRecents();
         }
 
         private void JsonOpenButton_Click(object sender, RibbonControlEventArgs e)
@@ -21,8 +24,45 @@ namespace ExcelEditorAddIn
             var result = openFileDialog1.ShowDialog();
             if (result == DialogResult.OK || result == DialogResult.Yes)
             {
-                Globals.ThisAddIn.OpenFile(openFileDialog1);
+                Globals.ThisAddIn.OpenFile(openFileDialog1.FileName);
             }
         }
+
+        #region Recents
+        private void InitRecents()
+        {
+            RecentsDropdown.SelectionChanged += RecentsDropdown_SelectionChanged;
+            RecentsDropdown.Label = string.Empty;
+
+            LoadRecents();
+
+            Recents.ItemUpdated += (_, __) => LoadRecents();
+        }
+
+        private void LoadRecents()
+        {
+            RecentsDropdown.Items.Clear();
+
+            var emptyItem = Factory.CreateRibbonDropDownItem();
+            emptyItem.Label = RecentLabel;
+            RecentsDropdown.Items.Add(emptyItem);
+
+            foreach (var recentData in Recents.Items)
+            {
+                var item = Factory.CreateRibbonDropDownItem();
+                item.Label = recentData.FilePath;
+                RecentsDropdown.Items.Add(item);
+            }
+        }
+
+        private void RecentsDropdown_SelectionChanged(object sender, RibbonControlEventArgs e)
+        {
+            var filePath = RecentsDropdown.SelectedItem.Label;
+            if (filePath != RecentLabel)
+            {
+                Globals.ThisAddIn.OpenFile(filePath);
+            }
+        }
+        #endregion
     }
 }
